6f0adc7232eb33444779dc5c299797a81de92d29
fayaass <muhammedfayas59504@gmail.com>
2025-01-24 10:14:43 +0530

    add

---
+++ list.py
@@ -0,0 +1,48 @@
+# 1
+
+# a=[]
+# b=[]
+# for i in range(1,101):
+#     a.append(i)
+#     if i%5 == 0:
+#         b.append(i)
+# print(a)
+# print(b)
+
+
+
+
+# numbers = list(range(1, 101))
+
+# for i in range(4, len(numbers), 5):
+#     number = numbers[i]
+#     square = number ** 2
+#     numbers[i] = square
+
+# print("\n", numbers)
+
+
+
+
+# 2
+
+# l = ['apple', 'mango', 'kiwi']
+# n = []
+
+# for word in l:
+#     if len(word) > 2:  
+#         n.append(word[2])  
+
+# print(n)
+
+
+
+l = ['apple', 'mango', 'kiwi']
+
+n = []
+
+
+for word in l:
+    n.append(word[2])  
+
+print(n)
